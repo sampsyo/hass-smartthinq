fa8f5fd6a5fa626f2a9cd9118f242fc473e5d5e9
wkd8176 <37936802+wkd8176@users.noreply.github.com>
2019-03-05 23:21:15 +0900

    Update climate.py

--- custom_components/smartthinq/climate.py
+++ custom_components/smartthinq/climate.py
@@ -17,7 +17,7 @@ from homeassistant.const import (
 import time
 import wideq
 
-REQUIREMENTS = ['wideq_kr == 0.0.2.dev0']
+REQUIREMENTS = ['wideq_kr == 0.0.2']
 DEPENDENCIES = ['smartthinq']
 
 LGE_HVAC_DEVICES = 'lge_HVAC_devices'
