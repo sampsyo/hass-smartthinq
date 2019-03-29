2f0d9de4bd0a28e4d80c0cd1c36c329196b82f23
wkd8176 <37936802+wkd8176@users.noreply.github.com>
2019-03-29 16:31:33 +0900

    Update __init__.py

--- custom_components/smartthinq/__init__.py
+++ custom_components/smartthinq/__init__.py
@@ -14,7 +14,7 @@ import homeassistant.helpers.config_validation as cv
 from homeassistant.helpers.entity import Entity
 
 
-REQUIREMENTS = ['wideq_kr == 0.0.8']
+REQUIREMENTS = ['wideq_kr == 0.0.9']
 
 DOMAIN = 'smartthinq'
 
