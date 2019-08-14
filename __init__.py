d98cab684b945af328e7d8d3c4ac6225203b5364
Dermot Duffy <dermot.duffy@gmail.com>
2019-08-13 20:12:38 -0700

    Dishwasher support for smartthinq platform.

---
+++ __init__.py
@@ -0,0 +1,57 @@
+"""
+Support for LG Smartthinq devices.
+"""
+import logging
+import wideq
+
+import voluptuous as vol
+
+from homeassistant.const import CONF_TOKEN
+import homeassistant.helpers.config_validation as cv
+from homeassistant.helpers import discovery
+from homeassistant.helpers.entity import Entity
+
+DOMAIN = 'smartthinq'
+
+CONFIG_SCHEMA = vol.Schema({
+    DOMAIN: vol.Schema({
+        vol.Required(CONF_TOKEN): cv.string,
+        })
+}, extra=vol.ALLOW_EXTRA)
+
+
+LOGGER = logging.getLogger(__name__)
+
+SMARTTHINQ_COMPONENTS = [
+    'sensor'
+]
+KEY_SMARTTHINQ_DEVICES = 'smartthinq_devices'
+
+def setup(hass, config):
+    if KEY_SMARTTHINQ_DEVICES not in hass.data:
+        hass.data[KEY_SMARTTHINQ_DEVICES] = []
+
+    refresh_token = config[DOMAIN][CONF_TOKEN]
+    client = wideq.Client.from_token(refresh_token)
+    hass.data[CONF_TOKEN] = refresh_token
+
+    for device in client.devices:
+        hass.data[KEY_SMARTTHINQ_DEVICES].append(device.id)
+
+    for component in SMARTTHINQ_COMPONENTS:
+        discovery.load_platform(hass, component, DOMAIN, {}, config)
+    return True
+
+
+class LGDevice(Entity):
+    def __init__(self, client, device):
+        self._client = client
+        self._device = device
+
+    @property
+    def name(self):
+        return self._device.name
+
+    @property
+    def available(self):
+        return True
